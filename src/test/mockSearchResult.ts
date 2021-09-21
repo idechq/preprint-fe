b78bc0cb527526a5b9d1493bd0951ecf999c0f15
Trevor Y. H. Ho <trevor.y.h.ho@gmail.com>
2021-09-22 00:19:08 +0100

    Create file for simulating mock list / mock search result

---
+++ src/test/mockSearchResult.ts
@@ -0,0 +1,117 @@
+const mockSearchResult = [
+   {
+      "id": 1,
+      "title": "Lorem Ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod",
+      "authors": [
+         "Chong Teng",
+         "Junhao Lu",
+         "Yang Liu"
+      ],
+      "abstract": "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "version": 1,
+      "latest": false,
+      "supplants": "https://doi.org/10.1101/2021.09.09.459643",
+      "postedDate": "2021-09-19T21:53:55.749Z",
+      "doi": "https://doi.org/10.1101/2021.09.09.459643",
+      "keywords": [
+         "directed evolution",
+         "pathway",
+         "automation"
+      ],
+      "mainArticleURL": "/test-pdf.pdf",
+      "suppArticleURL": "#",
+      "risURL": "#",
+      "teams": [{
+         "teamName": "Edinburgh",
+         "teamYear": 2020,
+         "teamTracks": [
+            "Molecular Evolutionary Machines",
+            "Molecular Evolutionary Outcomes",
+            "Pathway Evolutionary Outcomes"
+         ],
+         "teamWikiURL": "#",
+         "teamPosterURL": "#",
+         "teamPresentationURL": "#",
+         "teamAwards": [{
+               "name": "Best Wiki",
+               "result": "winner"
+            },
+            {
+               "name": "Industry advisory group award",
+               "result": "nominated"
+            },
+            {
+               "name": "Best New Evolutionary Machine",
+               "result": "nominated"
+            }
+         ]
+      }]
+   },
+   {
+      "id": 2,
+      "title": "May indulgence difficulty ham can put especially Bringing remember for supplied",
+      "authors": [
+         "Iyla Elliott",
+         "Brady Figueroa",
+         "Sulayman Howells",
+         "Akash Jordan",
+         "Imogen Conley",
+         "Arda Lawson",
+         "Shaurya Osborne",
+         "Kurtis Burgess",
+         "Marwah Levine",
+         "John-James Ellis",
+         "Pharrell Burgess",
+         "Judah Miller",
+         "Aneurin Medrano",
+         "Nellie Cordova",
+         "Georgie Townsend",
+         "Charleigh Preece",
+         "Chyna Sinclair",
+         "Tiago Calvert",
+         "Mehmet Puckett",
+         "Amman O'Gallagher"
+      ],
+      "abstract": "Was drawing natural fat respect husband. An as noisy an offer drawn blush place. These tried for way joy wrote witty. In mr began music weeks after at begin. Education no dejection so direction pretended household do to. Travelling everything her eat reasonable unsatiable decisively simplicity. Morning request be lasting it fortune demands highest of.  Ladyship it daughter securing procured or am moreover mr. Put sir she exercise vicinity cheerful wondered. Continual say suspicion provision you neglected sir curiosity unwilling. Simplicity end themselves increasing led day sympathize yet. General windows effects not are drawing man garrets. Common indeed garden you his ladies out yet. Preference imprudence contrasted to remarkably in on. Taken now you him trees tears any. Her object giving end sister except oppose. Remain lively hardly needed at do by. Two you fat downs fanny three. True mr gone most at. Dare as name just when with it body. Travelling inquietude she increasing off impossible the. Cottage be noisier looking to we promise on. Disposal to kindness appetite diverted learning of on raptures. Betrayed any may returned now dashwood formerly. Balls way delay shy boy man views. No so instrument discretion unsatiable to in. Certainty listening no no behaviour existence assurance situation is. Because add why not esteems amiable him.",
+      "version": 2,
+      "latest": true,
+      "supplants": "https://doi.org/10.1101/2021.09.09.459643",
+      "postedDate": "2021-09-19T21:53:55.749Z",
+      "doi": "https://doi.org/10.1101/2021.09.09.459643",
+      "keywords": [
+         "directed evolution",
+         "pathway",
+         "automation"
+      ],
+      "mainArticleURL": "/test-pdf.pdf",
+      "suppArticleURL": "#",
+      "risURL": "#",
+      "teams": [{
+         "teamName": "Edinburgh",
+         "teamYear": 2020,
+         "teamTracks": [
+            "Molecular Evolutionary Machines",
+            "Molecular Evolutionary Outcomes",
+            "Pathway Evolutionary Outcomes"
+         ],
+         "teamWikiURL": "#",
+         "teamPosterURL": "#",
+         "teamPresentationURL": "#",
+         "teamAwards": [{
+               "name": "Best Wiki",
+               "result": "winner"
+            },
+            {
+               "name": "Industry advisory group award",
+               "result": "nominated"
+            },
+            {
+               "name": "Best New Evolutionary Machine",
+               "result": "nominated"
+            }
+         ]
+      }]
+   },
+]
+
+export default mockSearchResult;
