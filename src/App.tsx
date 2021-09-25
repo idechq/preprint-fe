567aea3f1d8c0ea3cdf370c3046704baa4e2a084
Trevor Y. H. Ho <trevor.y.h.ho@gmail.com>
2021-09-26 00:05:47 +0100

    Add front page draft

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,28 @@ import {
   NavLink, 
   useLocation,
 } from 'react-router-dom';
+import Link from '@mui/material/Link'
 
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Drawer from '@mui/material/Drawer';
+import Stack from '@mui/material/Stack'
 
 import Container from '@mui/material/Container'
 import Paper from '@mui/material/Paper'
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import Icon from '@mdi/react';
+
+import {Card, CardActions, CardContent} from '@mui/material';
+import Button from '@mui/material/Button';
+
 import {
         mdiFormatListBulleted ,
         mdiCalendarClock,
@@ -55,13 +62,22 @@ import ArticleDisplayPage from './pages/ArticleDisplayPage';
 import { Acknowledgements, Terms } from './pages/StaticPages'
 import ScreenHeightDiv from './components/ScreenHeightDiv';
 
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import { 
+        mdiPresentation,
+        mdiTrophyVariant,
+      } from '@mdi/js';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+
+import KeyWordChip from './components/CustomChips';
+
+import Popper from '@mui/material/Popper';
+
 // Mock info
 import mockInfoJson from './test/mockInfo'
 const mockInfo = JSON.parse(JSON.stringify(mockInfoJson[0]));
 
 const mainMenueDrawerWidth = 248;
-
-// const styleX = theme.
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -201,32 +217,119 @@ function MainMenuItems () {
 }
 
 function Home() {
-  return (
-    <Box sx={{width: '100vw'}}>
-      <Grid container spacing={{ xs: 1, sm: 2}}>
-        <Grid item lg={4}>
-          <ScreenHeightDiv sx={{backgroundColor: 'blue'}}>
 
-          </ScreenHeightDiv>
+  const [showAbstract, setAbstractVisibility] = React.useState(false);
+
+  const viewOptions = (
+    <Stack sx={{paddingY: theme.spacing(2)}}spacing={theme.spacing(2)}>
+      <Box width="30%" mx="auto">
+      <Image
+        src="/logo512.png"
+        color="#ffffff00"
+        aspectRatio={1}
+        />
+      </Box>
+      <Typography variant="h5">This is the way the life begins. Not with a plan but a competition.</Typography>
+      
+      
+    </Stack>
+  )
+
+  const abstractWindow = (
+    <Box>
+      <Typography variant="h6" mt={2}>Abstract</Typography>
+      <Typography variant="body1">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+    </Box>
+  )
+
+  const articleCard = (
+    <Card sx={{ minWidth: 275, }}>
+            <CardContent>
+              {/* <Stack direction="row" justifyContent="space-between" alignItems="center"> */}
+                  {/* <Box padding={0} margin={0}> */}
+                  <Typography sx={{ fontSize: 14, fontWeight: "bold"}} color="text.secondary" display="inline">
+                    iDEC 2021 | Edinburgh
+                  </Typography>
+                  <Typography sx={{ fontSize: 12, }} color="text.secondary">
+                    Molecular Evolutionary Machines, Pathway Evolutionary Outcomes
+                  </Typography>
+                  {/* <Typography sx={{ fontSize: 14}} color="text.secondary" marginBottom={1}>
+                    Tracks: Molecular Evolutionary Machines, Molecular Evolutionary Outcomes, Pathway Evolutionary Outcomes
+                  </Typography> */}
+
+                  {/* <IconButton
+                    size="small"
+                    sx={{ml: 0.25,}}
+                  ><MenuBookOutlinedIcon sx={{fontSize: 15}}/></IconButton>
+                   <IconButton
+                    size="small"
+                  ><svg style={{ width:"15px", height: "15px",}} viewBox="0 0 24 22">
+                    <path fill="currentColor" d="M2,3H10A2,2 0 0,1 12,1A2,2 0 0,1 14,3H22V5H21V16H15.25L17,22H15L13.25,16H10.75L9,22H7L8.75,16H3V5H2V3M5,5V14H19V5H5Z" />
+                </svg></IconButton>
+                  <IconButton
+                    size="small"
+                  ><VideocamOutlinedIcon sx={{fontSize: 17}}/></IconButton> */}
+                  {/* 
+                  <Typography sx={{ fontSize: 14}} color="text.secondary" display="inline-flex" marginBottom={1}>|</Typography>
+                  </Box> */}
+                
+                {/* <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                  iDEC 2021 | Edinburgh
+                </Typography> */}
+              {/* </Stack> */}
+              {/* <Stack direction="row" spacing={1}>
+              <KeyWordChip label="Molecular Evolutionary Machines" />
+              <KeyWordChip label="Molecular Evolutionary Outcomes" />
+              <KeyWordChip label="Pathway Evolutionary Outcomes" />
+              </Stack> */}
+              <Typography component="div" sx={{ fontSize:18, fontWeight: "bold", mb: 0.5 }}>
+                <Link href="/article/mock-article" underline="hover">
+                Lorem Ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod sed do eiusmod Ipsum
+                </Link>
+              </Typography>
+              <Typography variant="body2">
+                "Iyla Elliott", "Brady Figueroa", "Sulayman Howells", "Akash Jordan", "Imogen Conley", "Arda Lawson", "Shaurya Osborne", "Kurtis Burgess", "Marwah Levine", "John-James Ellis", "Pharrell Burgess", "Judah Miller", "Aneurin Medrano", "Nellie Cordova", "Georgie Townsend", "Charleigh Preece", "Chyna Sinclair", "Tiago Calvert"
+              </Typography>
+            </CardContent>
+            {/* <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions> */}
+          </Card>
+  )
+
+  return (
+      <Container>
+      <Grid container spacing={0}>
+        <Grid item sm={12} md={3} lg={4}>
+        <Container>
+          {showAbstract? abstractWindow: viewOptions}
+        </Container>
         </Grid>
 
-        <Grid item lg={8}>
-          <Box sx={{padding: theme.spacing(4),
-                    backgroundColor: "grey",
-                    display: 'grid',
-          }}>
-          <Paper elevation={2}>
-              <Box sx= {{padding: theme.spacing(3)}}>
-              Testing
-              </Box>
-            </Paper>
+        <Grid item sm={12} md={9} lg={8}>
+          <Box
+            sx={{
+              padding: theme.spacing(2),
+              // paddingRight: theme.spacing(12),
+              // paddingLeft: theme.spacing(6),
+              // [theme.breakpoints.down('md')]: {
+              //   padding: theme.spacing(2),
+              // },
+            }}
+          >
+            <Stack spacing={theme.spacing(2)}>
+            {articleCard}{articleCard}{articleCard}
+            {articleCard}{articleCard}{articleCard}
+            {articleCard}{articleCard}{articleCard}
+            {articleCard}{articleCard}{articleCard}
+            </Stack>
           </Box>
         
       
         </Grid>
       
       </Grid>
-    </Box>
+      </Container>
   )
 }
 
@@ -302,7 +405,7 @@ export default function App() {
             </Toolbar>
           </AppBar>
 
-          <Box>
+          <Box sx={{ width: "100vw", backgroundColor: "#EAEEF3" }}>
             <DrawerHeader />
             <RouterSwitch>
               <Route path="/article/test">
