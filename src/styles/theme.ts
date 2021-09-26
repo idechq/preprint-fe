c1fcebf17000cdcd7ea42ccd258b13e56c9c56aa
Trevor Y. H. Ho <trevor.y.h.ho@gmail.com>
2021-09-26 01:58:48 +0100

    Save dark purple theme as backup

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,9 +4,12 @@ const theme = createTheme({
   // Material color chosen closest to iDEC themes chosen by Kening
   palette: {
     primary: {
-      main: '#7e57c2'},
+      main: '#7e57c2'
+      // main: "#2a206a",
+    },
     secondary: {
       main: '#43a047',
+      // main: "#00ad66",
     },
   },
 });
